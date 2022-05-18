ba76bcea26f0c9421a5a87f6dfe964ed27132a45
laradocs <75692074@qq.com>
2022-05-18 15:46:41 +0800

    Update

--- src/DTO/AppendParam.php
+++ src/DTO/AppendParam.php
@@ -16,12 +16,35 @@ use KY\TME\Json;
 class AppendParam implements ParamInterface
 {
     /**
-     * @param array $track
+     * @param string $trackName 单集名称(必填)
+     * @param null|string $transName 单集翻译名
+     * @param null|string $otherName 单集其他名
+     * @param int $location // 曲序，从1开始计数(必填)
+     * @param int $language // 语言(必填)
+     * @param int $trackNumber // 传输方用于唯一标识章节的编码(必填)
+     * @param string $publishTime // 发行时间(必填)
+     * @param null|string $saleTime // 上线时间
+     * @param string $audioUrl // 音频链接(必填)
+     * @param string $company // 传输方名(必填)
+     * @param string $number // 传输方用于唯一标识的书籍编码(必填)
+     * @param null|int $audioChange // 如果希望更新音频，则置为 1
+     *
      * @param array $singers
      */
     public function __construct(
-        public array $track,
-        public array $singers,
+        public string $trackName,
+        public ?string $transName = null,
+        public ?string $otherName = null,
+        public int $location,
+        public int $language,
+        public int $trackNumber,
+        public string $publishTime,
+        public ?string $saleTime = null,
+        public string $audioUrl,
+        public string $company,
+        public string $number,
+        public ?int $audioChange = null,
+        public object $singers,
     ) {
     }
 
@@ -34,8 +57,23 @@ class AppendParam implements ParamInterface
     {
         return [
             'info' => [
-                'track' => $this->track,
-                'track_singers' => $this->singers,
+                'track' => [
+                    'Flong_track_name' => $this->trackName,
+                    'Ftrans_name' => $this->transName,
+                    'Fother_name' => $this->otherName,
+                    'Flocation' => $this->location,
+                    'Flanguage' => $this->language,
+                    'Ftrack_number' => $this->trackNumber,
+                    'Fpublic_time' => $this->publishTime,
+                    'Fsale_time' => $this->saleTime,
+                    'Fori_audio_url' => $this->audioUrl,
+                    'Ftrans_company' => $this->company,
+                    'Flong_album_number' => $this->number,
+                    'Fis_audio_change' => $this->audioChange,
+                ],
+                'track_singers' => [
+                    $this->singers,
+                ],
             ],
         ];
     }
