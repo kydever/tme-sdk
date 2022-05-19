0998a6d8adba46ac97c7ca5e6c69c09126d47658
李铭昕 <715557344@qq.com>
2022-05-19 20:38:54 +0800

    增加错误码

---
+++ src/DTO/ErrorCode.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace KY\TME\DTO;
+
+class ErrorCode
+{
+    // 书籍没有入库
+    public const ALBUM_NOT_IMPORTED = 20003;
+}
